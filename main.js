8e35bb7c68722b5fe6db380d362cee3d2b4125c7
Jieck2002 <377592235@qq.com>
2018-04-14 21:04:22 +0800

    1.1 1.字体大小跟随容器缩放。 2.在代码中的速度修改位置增加注释。 3.新增cookie记录，可以用于记录上次浏览的容器以及浏览的位置，方便下次切换回该容器继续浏览。 4.修复部分BUG。

--- main.js
+++ main.js
@@ -3,20 +3,36 @@ var w=$(window).width();
 
 $(function(){
 
+//识别cookie
+
+if($.cookie("page_num") ==null){
+    $.cookie("page_num", "0", { expires: 7 });
+}else{
+    if($.cookie("page_num")>0){
+        $("#whole").css("margin-left",-w*$.cookie("page_num"));
+    }else{
+        $("#whole").css("margin-left",0);
+    }
+}
+
+
 //初始化
 
 $("#whole").width(w*3);
 $(".main").width(w);
-
+$("#whole").css({"transition": "all 1s",});
 
 })
 
 
-function change_page(num) {
+function change_page(num,onum) {
+    $.cookie("page_num", num, { expires: 7 });
+    $.cookie("page_"+(onum), $(".main").eq(onum).scrollTop());
     $(".main").width(w-100).css({
         "height":"80%",
         "margin-left":"20px",
         "box-shadow": "5px 5px 15px #c2c2c2",
+        "font-size": "80%",
     });
     if(num>0){
         $("#whole").css("margin-left",-w*num+num*110);
@@ -28,7 +44,9 @@ function change_page(num) {
             "height":"100%",
             "margin-left":"0px",
             "box-shadow": "none",
+            "font-size": "100%",
         });
         $("#whole").css("margin-left",-w*num);
-    },1000)
+        $(".main").eq(num).animate({scrollTop:$.cookie("page_"+num)},1000);/*在这里更改切换速度，除了这里，你还需要到CSS文件中更改相应参数*/
+    },1000)/*在这里更改切换速度，除了这里，你还需要到CSS文件中更改相应参数*/
 }
